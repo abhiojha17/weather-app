061b3ca09f68d4cea299d317e919a46868732ebb
abhiojha17 <74065758+abhiojha17@users.noreply.github.com>
2021-05-22 01:59:47 +0530

    Update app1.js

--- app1.js
+++ app1.js
@@ -62,6 +62,6 @@ function datefunction(d){
     let day=days[d.getDay()];
     let date=d.getDate();
     let month=months[d.getMonth()];
-    let year=d.getYear();
+    let year = d.getFullYear();
     return `${day}, ${date} ${month} ${year}`;
 }
